4d8c049b3abf9fc9fe6bd1fc67193018119e86d2
Dustin Griffith <djarekg@gmail.com>
2025-07-31 18:21:46 -0400

    basics

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 # Welcome to the Epic Games Interview Repo 🎉
-
-Each branch on this repo uses React 19 with a different framework or library. Below is a list of branches and their corresponding framework:
-
-## Repo
-
-<span style="font-size:1.7em;margin-right:20px">🍒</span>[epic-games-interview](https://github.com/djarekg/epic-games-interview)
-
-## Branches
-
-<div><span style="font-size:1.7em;margin-right:20px">🍬</span>React Router (v7)</div>
-<div><span style="font-size:1.7em;margin-right:20px">🍭</span>Next.js</div>
---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+import css from '@eslint/css';
+import js from '@eslint/js';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,ts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,ts,jsx,tsx}'],
+    plugins: { pluginReact },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    extends: ['plugin:react/jsx-runtime'],
+    å,
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.json5'],
+    plugins: { json },
+    language: 'json/json5',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended'],
+  },
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended'],
+  },
+]);
