a67e2bc10317b4b2257f0019d7f0a7340a7cf3e5
Dustin Griffith <djarekg@gmail.com>
2025-07-31 21:24:09 -0400

    removed welcome

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+import { useState } from 'react';
+import styles from './home.module.css';
 
 export default function Home() {
-  return <Welcome />;
+  const [result, setResult] = useState('N/A');
+
+  return (
+    <div className={styles.container}>
+      <span className={styles.result}>Result: {result}</span>
+    </div>
+  );
 }
